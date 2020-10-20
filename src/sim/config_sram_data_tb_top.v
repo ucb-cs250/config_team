cf569a07be9574ccc2fb157889c1849aef18c7b4
Aled Cuda <aledvirgil@gmail.com>
2020-10-19 23:23:26 -0700

    add docs and file structure

---
+++ src/sim/config_sram_data_tb_top.v
@@ -0,0 +1,8 @@
+`timescale 1ns/1ps
+
+`include "../behavioral/config_sram_data.v"
+
+module config_sram_data_tb_top;
+    initial begin
+    end
+endmodule // config_sram_data_tb_top
---
+++ src/sim/config_tile_tb_top.v
@@ -0,0 +1,8 @@
+`timescale 1ns/1ps
+
+`include "../behavioral/config_tile.v"
+
+module config_tile_tb_top;
+    initial begin
+    end
+endmodule // config_tile_tb_top
