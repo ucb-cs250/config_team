b13f9269df7e916def4e81550db1ded7723bf471
Aled Cuda <aledvirgil@gmail.com>
2020-10-22 11:29:45 -0700

    Add configuration for openlane

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@
 .PHONY: clean test_sram
 
 test_sram: config_sram_data_tb_top.gcd
+test_tile: config_tile_tb_top.gcd
 
 %.gcd: ./src/sim/%.v
 	iverilog $< && vvp a.out
---
+++ src/synthesis/config_tile_config.tcl
@@ -0,0 +1,20 @@
+# Design
+set ::env(DESIGN_NAME) "config_tile"
+
+set ::env(VERILOG_FILES) [$::env(OPENLANE_ROOT)/designs/config_team/src/behavioral/config_tile.v $::env(OPENLANE_ROOT)/designs/config_team/src/behavioral/REGISTER.v]
+
+set ::env(CLOCK_PERIOD) "1.0"
+set ::env(CLOCK_PORT) "clk"
+
+set ::env(RUN_MAGIC) 1
+set ::env(RUN_ROUTING_DETAILED) 1
+
+set ::env(CLOCK_NET) "clk"
+set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+
+# set ::env(FP_PIN_ORDER_CFG) $::env(OPENLANE_ROOT)/designs/config_tile/pin_order.cfg
+
+set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
