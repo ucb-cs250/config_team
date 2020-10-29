7545e99087276b597af2a4fd8b2de6ce130448a2
Aled Cuda <aledvirgil@gmail.com>
2020-10-28 19:23:34 -0700

    Add alternate tile config

--- src/behavioral/config_tile.v
+++ src/behavioral/config_tile.v
@@ -1,70 +1,62 @@
 module config_tile #(
-  parameter comb_N = 7,
-  parameter mem_N = 7
-  
+    parameter comb_N = 7,
+    parameter mem_N = 7
 ) (
-input clk,
-  input rst,
-  
-  output reg [comb_N-3:0] comb_config,
-  output reg [mem_N-1:0] mem_config,
-  
-  input set_soft,
-  input set_hard,
-  input shift_in_hard,
-  input shift_in_soft,
-    
-  output shift_out
+    input clk,
+    input rst,
+    input shift_enable,
+
+    output [comb_N-1:0] comb_config,
+    output [mem_N-1:0] mem_config,
+    output comb_set,
+    output mem_set,
+
+    input set_soft,
+    input set_hard,
+    input shift_in_hard,
+    input shift_in_soft,
+
+    output shift_out
 );
-  
-  initial comb_config = 0;
-  initial mem_config = 0;
-  
-  wire shift_en, set_config, mem_config_en;
-  wire [comb_N-1:0] comb_shift_out;
-  wire set_config_in = shift_en ? comb_shift_out[0] : set_config;
-  assign shift_en = set_config ? set_hard : set_soft;
-  
-  REGISTER #(.N(1)) set_config_reg (
-    .q(set_config_in),
-    .d(set_config),
-    .clk(clk),
-    .rst(rst)
-  );
-  
-  wire shift_in = set_config ? shift_in_hard : shift_in_soft;
-  wire mem_config_en_in = shift_en ? comb_shift_out[1] : mem_config_en;
-  
-  REGISTER #(.N(1)) mem_config_en_reg (
-    .q(mem_config_en_in),
-    .d(mem_config_en),
-    .clk(clk),
-    .rst(rst)
-  );
-  
-  wire [comb_N-3:0] stored_comb = shift_en ? comb_shift_out[comb_N-1:2] : stored_comb;
-  wire [comb_N-1:0] comb_shift_in = {comb_shift_out[comb_N - 2:0], shift_in};
-  
-  REGISTER #(.N(comb_N)) comb_shift (
-    .q(comb_shift_out),
-    .d(comb_shift_in),
-    .clk(clk),
-    .rst(rst)
-  );
-  
-  wire mem_clk = mem_config_en && clk;
-  wire [mem_N-1:0] mem_shift_out;
-  wire [mem_N-1:0] stored_mem = shift_en ? mem_shift_out[mem_N-1:0] : stored_mem;
-  wire [mem_N-1:0] mem_shift_in = {mem_shift_out[mem_N - 2:0], stored_comb[comb_N - 3]};
-  
-  REGISTER #(.N(mem_N)) mem_shift (
-    .q(mem_shift_out),
-    .d(mem_shift_in),
-    .clk(mem_clk),
-    .rst(rst)
-  );
-  
-  wire shift_out_final = mem_config_en ? mem_shift_out[mem_N - 1] : comb_shift_out[comb_N - 1];
-  
-  assign shift_out = shift_out_final;
+    wire set_internal;
+    wire shift_in_internal;
+
+    wire mem_ctrl;
+    wire input_mux;
+
+    assign shift_in_internal = input_mux ? shift_in_soft : shift_in_hard;
+    assign set_internal = input_mux ? set_soft : set_hard;
+
+    assign mem_set = (~mem_ctrl) & set_internal;
+
+    wire [1:0] internal_config_inter;
+
+    config_latch #(.LENGTH(2)) internal_config (
+        .clk(clk),
+        .rst(rst),
+        .set(set_internal),
+
+        .shifter_data(internal_config_inter),
+        .config_bits({input_mux, mem_ctrl})
+    );
+
+    wire comb_mem_bridge;
+
+    shift_chain #(.LENGTH(comb_N + 2)) comb_shifter (
+        .clk(clk),
+        .rst(rst),
+        .shift_enable(shift_enable),
+        .shift_in(shift_in_internal),
+        .shift_out(comb_mem_bridge),
+        .config_data({internal_config_inter, comb_config})
+    );
+
+    shift_chain #(.LENGTH(mem_N)) mem_shifter (
+        .clk(clk),
+        .rst(rst),
+        .shift_enable(shift_enable),
+        .shift_in(comb_mem_bridge),
+        .shift_out(shift_out),
+        .config_data(mem_config)
+    );
 endmodule
--- src/sim/config_tile_tb_top.v
+++ src/sim/config_tile_tb_top.v
@@ -1,29 +1,36 @@
 `timescale 1ns/1ps
 
+`include "./src/behavioral/config_tile.v"
+`include "./src/behavioral/config_latch.v"
+`include "./src/behavioral/shift_chain.v"
+
 module config_tile_tb_top;
 
-  reg clk, set_soft, set_hard, shift_in_hard, shift_in_soft, rst;
+  reg clk, set_soft, set_hard, shift_in_hard, shift_in_soft, rst, shift_enable;
   wire shift_out;
   wire [4:0] cconfig;
   reg [4:0] prev_stored_comb;
-  wire [6:0] mconfig;
-  reg [6:0] prev_stored_mem;
+  wire [4:0] mconfig;
+  reg [4:0] prev_stored_mem;
 
-  wire [13:0] test_sequence = 56'b10101011010000111111110100101010001110011001010100010101;
-  config_tile DUT(
-    .clk(clk), 
-    .comb_config(cconfig), 
-    .mem_config(mconfig), 
-    .set_soft(set_soft), 
-    .set_hard(set_hard), 
-    .shift_in_hard(shift_in_hard), 
-    .shift_in_soft(shift_in_soft), 
-    .shift_out(shift_out), 
-    .rst(rst)
+  wire [12:0] test_sequence_hard = 12'b101010110100;
+  wire [12:0] test_sequence_soft = 12'b000111010001;
+  config_tile #(.comb_N(5), .mem_N(5)) DUT(
+    .clk(clk),
+    .comb_config(cconfig),
+    .mem_config(mconfig),
+    .set_soft(set_soft),
+    .set_hard(set_hard),
+    .shift_in_hard(shift_in_hard),
+    .shift_in_soft(shift_in_soft),
+    .shift_out(shift_out),
+    .rst(rst),
+    .shift_enable(shift_enable)
   );
 
   initial clk = 0;
   initial rst = 1;
+  initial shift_enable = 0;
 
   always #4 clk = ~clk;
 
@@ -34,102 +41,50 @@ module config_tile_tb_top;
   initial begin
     $dumpfile("tile_config_tb.vcd");
     $dumpvars;
-    set_soft = 1; 
-    set_hard = 1;
-    shift_in_hard = 0;
-    shift_in_soft = 0;
+    set_soft <= 1; 
+    set_hard <= 1;
+    shift_in_hard <= 0;
+    shift_in_soft <= 0;
     #16;
-    rst = 0;
+    rst <= 0;
     set_soft <= 0;
     set_hard <= 0;
-    for (i = 0; i < 7; i = i + 1) begin //Load bits into comb shift register
-      shift_in_soft <= test_sequence[i];
-      if ((cconfig !== 0) || (mconfig !== 0) || (shift_out !== 0)) begin
-        $display("Mismatches when loading comb test_sequence bit %d nonzero shift_out=%b comb_config=%h, mem_config=%h", i, shift_out, cconfig, mconfig);
-      end
+
+    // Test hard wired input on reset
+    shift_enable <= 1;
+    for (i = 0; i < 12; i = i + 1) begin
+      shift_in_hard <= test_sequence_hard[11 - i];
       #8;
     end
-
-    for (i = 7; i < 14; i = i + 1) begin //Test shift_out from comb shift register
-      shift_in_soft <= test_sequence[i];
-      if ((cconfig !== 0) || (mconfig !== 0)) begin
-        $display("Mismatches when loading test_sequence nonzero comb_config=%h, mem_config=%h", cconfig, mconfig);
-      end
-      if (shift_out !== test_sequence[i-7]) begin
-        $display("Mismatches at bit %d where shift_out=%b when it should be %b", i-7, shift_out, test_sequence[i-7]);
-      end
-      #8;
-    end
-
-    set_soft <= 1;
-
-    for (i = 14; i < 21; i = i + 1) begin //Load bits into mem shift register
-      prev_stored_comb <= cconfig;
-      shift_in_soft <= test_sequence[i];
-      if ((cconfig !== prev_stored_comb) || (mconfig !== 0) || (shift_out !== 0)) begin
-        $display("Mismatches when loading mem test_sequence bit %d nonzero shift_out=%b or mem_config=%h or changed comb_config=%h instead of %h", i-14, shift_out, mconfig, cconfig, prev_stored_comb);
-      end
-      #8;
-      set_soft <= 0;
-    end
-
-    for (i = 21; i < 28; i = i + 1) begin //Test shift_out from mem shift register
-      shift_in_soft <= test_sequence[i];
-      if ((cconfig !== prev_stored_comb) || (mconfig !== 0)) begin
-        $display("Mismatches when loading test_sequence changed comb_config=%h instead of %h or nonzero mem_config=%h", cconfig, prev_stored_comb, mconfig);
-      end
-      if (shift_out !== test_sequence[i-14]) begin
-        $display("Mismatches at bit %d where shift_out=%b when it should be %b", i-14, shift_out, test_sequence[i-14]);
-      end
-      set_soft = 1;
-      #8;
-    end
-
-    prev_stored_comb <= cconfig;
-    prev_stored_mem <= mconfig;
-    set_soft <= 0;
-
-    for (i = 28; i < 35; i = i + 1) begin //Load bits into comb shift register
-      shift_in_soft <= test_sequence[i];
-      if ((cconfig !== prev_stored_comb) || (mconfig !== prev_stored_mem) || (shift_out !== 0)) begin
-        $display("Mismatches when loading comb test_sequence bit %d nonzero shift_out=%b comb_config=%h, mem_config=%h", i, shift_out, cconfig, mconfig);
-      end
-      #8;
-    end
-
-    for (i = 35; i < 42; i = i + 1) begin //Test shift_out from comb shift register
-      shift_in_soft <= test_sequence[i];
-      if ((cconfig !== 0) || (mconfig !== 0)) begin
-        $display("Mismatches when loading test_sequence nonzero comb_config=%h, mem_config=%h", cconfig, mconfig);
-      end
-      if (shift_out !== test_sequence[i-7]) begin
-        $display("Mismatches at bit %d where shift_out=%b when it should be %b", i-7, shift_out, test_sequence[i-7]);
-      end
-      #8;
-    end
-
+    shift_enable <= 0;
     set_hard <= 1;
-
-    for (i = 42; i < 49; i = i + 1) begin //Load bits into mem shift register
-      prev_stored_comb <= cconfig;
-      shift_in_soft <= test_sequence[i];
-      if ((cconfig !== prev_stored_comb) || (mconfig !== 0) || (shift_out !== 0)) begin
-        $display("Mismatches when loading mem test_sequence bit %d nonzero shift_out=%b or mem_config=%h or changed comb_config=%h instead of %h", i-14, shift_out, mconfig, cconfig, prev_stored_comb);
-      end
-      #8;
-      set_hard <= 0;
+    #8;
+    set_hard <= 0;
+    if (cconfig !== test_sequence_hard[4:0]) begin
+      $display("Mismatch between combinatorial output, and input bits: comb %h, input %h", cconfig, test_sequence_hard[4:0]);
     end
+    if (mconfig !== test_sequence_hard[9:5]) begin
+      $display("Mismatch between memory output, and input bits: mem %h, input %h", mconfig, test_sequence_hard[9:5]);
+    end
+    #16;
 
-    for (i = 49; i < 56; i = i + 1) begin //Test shift_out from mem shift register
-      shift_in_soft <= test_sequence[i];
-      if ((cconfig !== prev_stored_comb) || (mconfig !== 0)) begin
-        $display("Mismatches when loading test_sequence changed comb_config=%h instead of %h or nonzero mem_config=%h", cconfig, prev_stored_comb, mconfig);
-      end
-      if (shift_out !== test_sequence[i-14]) begin
-        $display("Mismatches at bit %d where shift_out=%b when it should be %b", i-14, shift_out, test_sequence[i-14]);
-      end
+    // Switch to soft chain
+    shift_enable <= 1;
+    for (i = 0; i < 12; i = i + 1) begin
+      shift_in_hard <= test_sequence_soft[11 - i];
       #8;
     end
+    shift_enable <= 0;
+    set_hard <= 1;
+    #8;
+    set_soft <= 0;
+    if (cconfig !== test_sequence_soft[9:5]) begin
+      $display("Mismatch between combinatorial output, and input bits: comb %h, input %h", cconfig, test_sequence_soft[9:5]);
+    end
+    if (mconfig !== test_sequence_soft[4:0]) begin
+      $display("Mismatch between memory output, and input bits: mem %h, input %h", mconfig, test_sequence_soft[4:0]);
+    end
+    #16;
 
 
     $finish;
